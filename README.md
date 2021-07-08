95648a32c6eaaf30c44f1d0414561de866d4cc84
td31 <trdardani@gmail.com>
2021-07-07 22:55:31 -0400

    front end cleanup

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Built using [Docusaurus 2](https://docusaurus.io/) with continuous deployment through Cloudflare
 
 
-Added: 3
+Added: 6
 
-Completed but need to add: 49
+Completed but need to add: 46
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,46 +4,27 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    title: "Leetcode Easy",
+    img_src: "../../static/img/500287ld.jpg",
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
+    title: "Leetcode Medium",
+    img_src: "../../static/img/510803ld.jpg",
   },
   {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    title: "Leetcode Hard",
+    img_src: "../../static/img/515500ld.jpg",
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ img_src, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
+        <img className={styles.img_homepage} src={img_src} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
-        <p>{description}</p>
       </div>
     </div>
   );
